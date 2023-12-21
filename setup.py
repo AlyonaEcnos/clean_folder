9d568e691fcb08b12b1176e1485abdb6c537f065
Alyona <ecnosss@gmail.com>
2023-12-21 10:37:22 +0200

    clean_folder v3

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='clean_folder',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
+      entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']},
       packages=['clean_folder'],
       zip_safe=False)
