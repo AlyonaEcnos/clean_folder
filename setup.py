12bb6c30719e0c8184b8fc77a33bbc10bb22ba9b
Alyona <ecnosss@gmail.com>
2023-12-21 10:10:51 +0200

    clean_folder

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='usefull',
-      version='0.1',
+setup(name='clean_folder',
+      version='0.0.1',
       description='Very usefull code',
-      url='http://github.com/storborg/usefull',
+      url='https://github.com/AlyonaEcnos/clean_folder',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
-      packages=['usefull'],
+      packages=['clean_folder'],
       zip_safe=False)
